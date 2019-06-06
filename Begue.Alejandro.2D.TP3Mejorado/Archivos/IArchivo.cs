ac17fa6731552d418f45baf8722c3116ea98ac02
aleb1502 <Alejandro@pcALEB.(none)>
2019-06-06 19:34:57 -0300

    first commit

---
+++ Begue.Alejandro.2D.TP3Mejorado/Archivos/IArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public interface IArchivo<T>
+    {
+        /// <summary>
+        /// Metodo Guardar
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        bool Guardar(string archivo, T datos);
+
+        /// <summary>
+        /// Metodo Leer
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        bool Leer(string archivo, out T datos);
+    }
+}
---
+++ Begue.Alejandro.2D.TP3Mejorado/Excepciones/SinProfesorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class SinProfesorException : Exception
+    {
+        /// <summary>
+        /// Llama al de base para lanzar el mensaje
+        /// </summary>
+        public SinProfesorException() : base("No hay Profesor para la clase.")
+        {
+
+        }
+    }
+}
